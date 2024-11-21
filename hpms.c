a1c2fde1ec7c302a45da3198e4e7ddfb96fa2aea
Hassan Raza <hassanrazlakhani456@gmail.com>
2024-11-21 19:57:57 +0500

    Added file handling

--- hpms.c
+++ hpms.c
@@ -1,6 +1,37 @@
+#include <stddef.h>
 #include <stdio.h>
+
+void checkData();
+
 
 int main(void) {
   printf("Hello, World!");
+  
+  checkData();
+
   return 0;
+}
+
+void checkData()
+{
+  FILE *patPtr = fopen("patient.txt", "r");
+
+  if (patPtr == NULL)
+  {
+    fprintf(stderr, "The patient data file does not exist, creating file\n");
+    FILE *patPtr = fopen("patient.txt", "w");
+    fprintf(patPtr, "Init Patient Data\n");
+    fclose(patPtr);
+  }
+  
+
+  FILE *docPtr = fopen("doctors.txt", "r");
+
+  if (docPtr == NULL)
+  {
+    fprintf(stderr, "The doctors data file does not exist, creating file\n");
+    FILE *docPtr = fopen("doctors.txt", "w");
+    fprintf(docPtr, "Init Doctors Data\n");
+    fclose(docPtr);
+  }
 }
